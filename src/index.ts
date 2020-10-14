1e947fe0906fc0bb53262ae3822e3453c4cb9bb7
nooneexpectme <nooneexpectme@gmail.com>
2020-10-15 00:57:50 +0200

    ✨ Add fetch-cookie support

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
-import { RequestOptions } from 'https'
-import request from './Requestor'
+import nodeFetch, { RequestInfo, RequestInit } from 'node-fetch'
 import DataModeler, { ISelector } from './DataModeler'
 
+type ConfigureOptions = {
+  enableCookies: boolean
+}
+type QueryInfo = RequestInfo | (RequestInit & { url: RequestInfo })
+
+let fetch = nodeFetch
+
+export async function configure(options: ConfigureOptions): Promise<void> {
+  if (options.enableCookies) {
+    try {
+      const { default: fetchCookie } = await require('fetch-cookie/node-fetch')
+      fetch = fetchCookie(nodeFetch) as typeof nodeFetch
+    } catch (e) {
+      fetch = nodeFetch
+      console.error(
+        new Error('Please run `npm install fetch-cookie` to use this setting.')
+      )
+    }
+  } else {
+    fetch = nodeFetch
+  }
+}
+
 export async function ScrapeTA(
-  query: string | URL | RequestOptions,
+  query: QueryInfo,
   schema: { [key: string]: ISelector }
 ): Promise<unknown> {
-  const queryHTML = await request(query)
-  const dataModeler = new DataModeler(queryHTML)
+  const requestInfo = ((typeof query === 'object' && 'url' in query && query.url) ||
+    query) as RequestInfo
+  const requestInit = typeof query === 'object' ? (query as RequestInit) : undefined
+  const req = await fetch(requestInfo, requestInit)
+  const res = await req.text()
+  const dataModeler = new DataModeler(res)
   return dataModeler.generate(schema)
 }
