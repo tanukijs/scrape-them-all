967d94376f91e3a5ffacbe4e3168e333500b78f3
nooneexpectme <nooneexpectme@gmail.com>
2020-10-14 13:53:40 +0200

    ♻️ indent 2 spaces

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@ import req from './request'
 import transform from './transform'
 
 export interface IScrapeOptions {
-    selector?: string
-    isListItem?: boolean
-    isTrimmed?: boolean
-    attribute?: string
-    accessor?: string | ((node: cheerio.Cheerio) => string | number | boolean)
-    transformer?: (x: unknown) => typeof x
-    dataModel?: Record<string, string | IScrapeOptions>
+  selector?: string
+  isListItem?: boolean
+  isTrimmed?: boolean
+  attribute?: string
+  accessor?: string | ((node: cheerio.Cheerio) => string | number | boolean)
+  transformer?: (x: unknown) => typeof x
+  dataModel?: Record<string, string | IScrapeOptions>
 }
 
 export interface ISchema {
-    [key: string]: string | IScrapeOptions
+  [key: string]: string | IScrapeOptions
 }
 
 export default async function ScrapeTA(
-    url: string,
-    schema: ISchema
+  url: string,
+  schema: ISchema
 ): Promise<IScrapeOptions> {
-    const target = await req(url)
-    return transform(target, schema)
+  const target = await req(url)
+  return transform(target, schema)
 }
--- src/transform.ts
+++ src/transform.ts
@@ -13,87 +13,84 @@ import { IScrapeOptions } from '.'
  */
 
 function normalizeRules(item: Record<string, string | IScrapeOptions>): IScrapeOptions {
-    const rules = {}
+  const rules = {}
+  const isObject = typeof item === 'object' && item !== null
 
-    const isObject = typeof item === 'object' && item !== null
+  Object.defineProperty(rules, 'selector', {
+    value: !isObject ? item : item.selector || null
+  })
+  Object.defineProperty(rules, 'isListItem', {
+    value: !isObject ? false : item.isListItem || false
+  })
+  Object.defineProperty(rules, 'isTrimmed', {
+    value: !isObject ? true : item.isTrimmed || true
+  })
+  Object.defineProperty(rules, 'attribute', {
+    value: !isObject ? null : item.attribute || null
+  })
+  Object.defineProperty(rules, 'accessor', {
+    value: !isObject ? 'text' : item.accessor || 'text'
+  })
+  Object.defineProperty(rules, 'transformer', {
+    value: !isObject ? null : item.transformer || null
+  })
+  Object.defineProperty(rules, 'dataModel', {
+    value: !isObject ? null : item.dataModel || null
+  })
 
-    Object.defineProperty(rules, 'selector', {
-        value: !isObject ? item : item.selector || null
-    })
-    Object.defineProperty(rules, 'isListItem', {
-        value: !isObject ? false : item.isListItem || false
-    })
-    Object.defineProperty(rules, 'isTrimmed', {
-        value: !isObject ? true : item.isTrimmed || true
-    })
-    Object.defineProperty(rules, 'attribute', {
-        value: !isObject ? null : item.attribute || null
-    })
-    Object.defineProperty(rules, 'accessor', {
-        value: !isObject ? 'text' : item.accessor || 'text'
-    })
-    Object.defineProperty(rules, 'transformer', {
-        value: !isObject ? null : item.transformer || null
-    })
-    Object.defineProperty(rules, 'dataModel', {
-        value: !isObject ? null : item.dataModel || null
-    })
-
-    return rules
+  return rules
 }
 
 function processSingleItem($: cheerio.Root, rules: IScrapeOptions): IScrapeOptions {
-    const node = $(rules.selector)
-    let value =
-        typeof rules.accessor === 'function'
-            ? rules.accessor(node)
-            : typeof rules.accessor === 'string'
-            ? node[rules.accessor]()
-            : null
+  const node = $(rules.selector)
+  let value =
+    typeof rules.accessor === 'function'
+      ? rules.accessor(node)
+      : typeof rules.accessor === 'string'
+      ? node[rules.accessor]()
+      : null
 
-    if (rules.attribute !== null) value = node.attr(rules.attribute as string)
+  if (rules.attribute !== null) value = node.attr(rules.attribute as string)
+  if (rules.isTrimmed && value) value = value.trim()
+  if (typeof rules.transformer === 'function') value = rules.transformer(value)
 
-    if (rules.isTrimmed && value) value = value.trim()
-
-    if (typeof rules.transformer === 'function') value = rules.transformer(value)
-
-    return value
+  return value
 }
 
 function processMultipleItems(
-    $: cheerio.Root,
-    rules: IScrapeOptions
+  $: cheerio.Root,
+  rules: IScrapeOptions
 ): Promise<IScrapeOptions>[] {
-    const nodes = $(rules.selector).toArray()
-    const values = []
+  const nodes = $(rules.selector).toArray()
+  const values = []
 
-    for (const node of nodes) {
-        const value = transform(node, rules.dataModel)
-        values.push(value)
-    }
+  for (const node of nodes) {
+    const value = transform(node, rules.dataModel)
+    values.push(value)
+  }
 
-    return values
+  return values
 }
 
 export default async function transform(
-    sourceHTML: cheerio.Element,
-    dataModel: Record<string, string | IScrapeOptions> | undefined
+  sourceHTML: cheerio.Element,
+  dataModel: Record<string, string | IScrapeOptions> | undefined
 ): Promise<IScrapeOptions> {
-    const $ = cheerio.load(sourceHTML)
-    const mappedResult: { [key: string]: IScrapeOptions | IScrapeOptions[] } = {}
+  const $ = cheerio.load(sourceHTML)
+  const mappedResult: { [key: string]: IScrapeOptions | IScrapeOptions[] } = {}
 
-    for (const key in dataModel) {
-        const rules = normalizeRules(
-            dataModel[key] as Record<string, string | IScrapeOptions>
-        )
-        if (rules.selector === null) continue
+  for (const key in dataModel) {
+    const rules = normalizeRules(
+      dataModel[key] as Record<string, string | IScrapeOptions>
+    )
+    if (rules.selector === null) continue
 
-        const value = !rules.isListItem
-            ? processSingleItem($, rules)
-            : processMultipleItems($, rules)
+    const value = !rules.isListItem
+      ? processSingleItem($, rules)
+      : processMultipleItems($, rules)
 
-        mappedResult[key] = await (Array.isArray(value) ? Promise.all(value) : value)
-    }
+    mappedResult[key] = await (Array.isArray(value) ? Promise.all(value) : value)
+  }
 
-    return mappedResult
+  return mappedResult
 }
